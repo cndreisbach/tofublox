6fa16a6a19535e6689811f8dd4d5584bcd6fb002
Clinton R. Nixon <crnixon@gmail.com>
2009-03-10 21:47:23 -0400

    added docs to config file

--- config/tofu_config.rb
+++ config/tofu_config.rb
@@ -1,6 +1,14 @@
 Tofu.configure do |config|
+  
+   # Your site title
   config.title = "TofuBlox"
-  config.database = File.expand_path(Tofu.dir("system/tofu.db"))
-  config.admin_password = 'lombardo'
+  
+  # The password to get into your admin backend
+  config.admin_password = 'lombardo' 
+  
+  # How many blocks to show on the index page
   config.blocks_per_page = 7
+  
+  # Where your super-secret SQLite database is stored
+  config.database = File.expand_path(Tofu.dir("system/tofu.db"))
 end
